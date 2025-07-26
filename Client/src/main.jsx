ed2999915a26f1e9ffd639712a746a5486ba1673
JoyM268 <125041935+JoyM268@users.noreply.github.com>
2025-07-26 11:51:30 +0530

    Fixed deployement error in main

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App.jsx";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import { BrowserRouter } from "react-router-dom";
-import { CurrencyProvider } from "./context/currencyContext.jsx";
+import { CurrencyProvider } from "./context/CurrencyContext.jsx";
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
